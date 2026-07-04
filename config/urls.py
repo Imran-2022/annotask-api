c49a6886e0093480d2c2335b7ad84b0fcdf4240f
MD IMRANUL HAQUE <mdimranulhaque27@gmail.com>
2026-07-04 20:45:48 +0600

    Serve media files in production for uploaded medical images

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.http import JsonResponse
+from django.views.static import serve
 
 def root_view(request):
     return JsonResponse({
@@ -25,4 +26,11 @@ urlpatterns = [
     path('api/auth/', include('apps.users.urls')),
     path('api/tasks/', include('apps.tasks.urls')),
     path('api/', include('apps.annotations.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
